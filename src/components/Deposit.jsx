55e48b596e70add7cd7d4367e88a77780cf2b429
Harsh Mittal <harshmittal1750@gmail.com>
2022-11-12 11:18:22 +0530

    setuo rrd , removed unwanted things, added pages

---
+++ src/components/Deposit.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PriceForm from "./PriceForm";
+const Deposit = () => {
+  return (
+    <div>
+      Deposit
+      <PriceForm />
+    </div>
+  );
+};
+
+export default Deposit;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "./index.css";
+
+import reportWebVitals from "./reportWebVitals";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Deposit from "./components/Deposit";
+import PriceForm from "./components/PriceForm";
+import CreateProfile from "./components/CreateProfile";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="home" element={<App />} />
+        <Route path="deposit" element={<Deposit />} />
+        <Route path="price-menu" element={<PriceForm />} />
+        <Route path="create" element={<CreateProfile />} />
+        <Route path="nav" element={<Nav />} />
+        <Route path="header" element={<Header />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
